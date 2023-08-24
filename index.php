70d0381d11690974d8a9c76082f40ad2955936b2
Fadel Rahmatullah <fadelrahmatullah97@gmail.com>
2023-08-24 10:18:13 +0700

    commit mysql php

---
+++ index.php
@@ -0,0 +1,441 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bina Analitika</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&family=PT+Serif&display=swap"
+        rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=PT+Sans+Narrow:wght@700&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+    <!-- Navbar -->
+        <nav class="navbar">
+            <div class="row">
+                <div class="judul-nav">
+                    <img src="img/logo_fix.png" alt="none">
+                    <h1>Research Consultant & Data Analyst </h1>
+                </div>
+            </div>
+            <div class="navbar-nav">
+                <a href="#homes">Home</a>
+                <a href="#class">Online Class</a>
+                <a href="#data">Data Analyst</a>
+                <a href="#refrence">Reference</a>
+                <a href="#journal">Journal</a>
+                <a href="#about">About Us</a>
+            </div>
+            <div class="extra"><a id="menu" href="#menu"><img src="img/menu.svg" width="18" height="18"
+                        alt="hamburger"></a>
+                    </div>
+        </nav>
+
+
+        <!-- HOME -->
+        <section class="homes" id="homes">
+            <div class="col-home">
+                <div class="row-homes">
+                    <div class="homes-col">
+                        <div>
+                            <h3>Bimbingan<br> Penelitian dan <br>Analisis Data</h3>
+                            <p class="home-teks">Selesaikan Karya Ilmiah seperti Skripsi,<br>
+                                Tesis dan Disertasi secara Cepat dengan<br>
+                                Mengikuti Kelas Bimbingan dan Analisis <br>
+                                Data menggunakan berbagai Program<br>
+                                Statistik
+                            </p>
+                        </div>
+                        <div class="homes-footer">
+                            <div class="logo-home">
+                                <a href="https://www.instagram.com/bina.analitika/" target="_blank">
+                                    <img src="img/instagram.png" alt="none">
+                                </a>
+                                <a href="https://mail.google.com/" target="_blank">
+                                    <img src="img/gmail.png" alt="none">
+                                </a>
+                                <a href="#" target="_blank">
+                                    <img src="img/facebook.png" alt="none">
+                                </a>
+                                <a href="https://wa.me/6281326852905" target="_blank">
+                                    <img src="img/whatsapp.png" alt="none">
+                                </a>
+                            </div>
+                            <div class="direct-footer">
+                                <h6>@2023, Bina Analitika</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="homes-images">
+                        <div class="home-page">
+                            <img alt="none." class="block" src="img/img-home.png" />
+                        </div>
+                        <div class="img-footer">
+                            <img src="img/4x6.png" alt="">
+                        </div>
+                        <div class="teks-footer">
+                            <h5>Ropingun Muhlison, S.pd</h5>
+                            <p>
+                                Menguasai keilmuan dalam bidang Penelitian Matematika,<br>
+                                Marketing, Business Strategy, MSDM, Ekonomi, Akuntansi
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- Online Class -->
+        <section class="class" id="class">
+            <a href="#class">Online Class</a>
+            <div class="row-class">
+                <div class="col-class">
+                    <a href="https://wa.me/6285722431760" target="_blank">
+                        <img src="img/class-img1.png" alt="none">
+                    </a>
+                    <p>
+                        Bimbingan online dapat melalui <br>
+                        Zoom dan GMeet dengan tarif <br>
+                        Rp. 100.000/jam
+                    </p>
+                </div>
+                <div class="col-class">
+                    <a href="https://wa.me/6285722431760" target="_blank">
+                        <img src="img/class-img2.png" alt="none">
+                    </a>
+                    <p>
+                        Bimbingan tatap muka dilakukan <br>
+                        di Area Jakarta Barat dengan <br>
+                        tarif Rp. 100.000/jam ditambah <br>
+                        uang jalan
+                    </p>
+                </div>
+            </div>
+        </section>
+
+        <!-- Data Analyst -->
+        <section class="data" id="data">
+            <a href="#class">Data Analyst</a>
+            <p>Tarif Analisis data bervariasi tergantung kompleksitas model dan metode analisis yang digunakan <br>
+                Analisis data dengan SPSS berkisar antara Rp. 200.000 - Rp. 1.000.0000 <br>
+                Analisis data dengan Eviews berkisar antara Rp. 400.000 - Rp. 1.500.000 <br>
+                Analisis data dengan Amos & SmartPLS berkisar antara Rp. 600.000 - Rp. 2.000.000</p>
+            <div class="row-data">
+                <div class="col-data-img1">
+                    <img src="img/spss.png" alt="none">
+
+                </div>
+                <div class="col-data-img2">
+                    <img src="img/eviews.png" alt="none">
+                </div>
+                <div class="col-data-img3">
+                    <img src="img/pls.png" alt="none">
+
+                </div>
+            </div>
+            <div class="row-data-2">
+                <div class="col-data-2">
+                    <h3>SPSS</h3>
+                    <p>
+                        Analisis Deskriptif <br>
+                        Analisis Univariat <br>
+                        Analisis Bivariat <br>
+                        Analisis Multivariat <br>
+                        Analisis Korelasi <br>
+                        Analisis Regresi Linear <br>
+                        Analisis Regresi Moderasi <br>
+                        Analisis Jalur <br>
+                    </p>
+                </div>
+                <div class="col-data-2">
+                    <h3>Eviews</h3>
+                    <p>
+                        Analisis Regresi Data Panel <br>
+                        Analisis VECM <br>
+                        Analisis ADRL <br>
+                    </p>
+                </div>
+                <div class="col-data-2">
+                    <h3>Amos & SmartPLS</h3>
+                    <p>
+                        Analisis SEM dengan Amos <br>
+                        Analisis PLS dengan SmartpPLS <br>
+                    </p>
+                </div>
+            </div>
+        </section>
+
+
+        <!-- Reference -->
+        <section class="refrence" id="refrence">
+            <a href="#refrence">Reference</a>
+            <div class="row-ref">
+                <div class="col-ref">
+                    <h3>E-Book</h3>
+                    <p>Bina Analitika menyediakan berbagai <br>
+                        referensi dalam bentuk buku <br>
+                        elektronik (e-book) untuk bidang ilmu berikut.</p>
+                    <div class="ref-content">
+                        <p>
+                            1. Pemasaran <br>
+                            2. Manajemen <br>
+                            3. Metodology <br>
+                            4. Analisis Data <br>
+                            5. dst. <br>
+                        </p>
+                    </div>
+                </div>
+                <div class="col-ref">
+                    <h3>Journal</h3>
+                    <p>
+                        Bina Analitika menyediakan <br>
+                        berbagai referensi dalam bentuk <br>
+                        jurnal terindeks scopus beserta <br>
+                        terjemahan dari berbagai sumber berikut.
+                    </p>
+                    <div class="ref-content">
+                        <p>
+                            1. Elsavier <br>
+                            2. Emerald <br>
+                            3. dst. <br>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- JOURNAL -->
+        <section class="journal" id="journal">
+            <a href="#journal">Journal Publisity</a>
+            <div class="jr-content">
+                <div class= "col-jr">
+                    <p>
+                        Bina Analitika membantu dalam pembuatan <br>
+                        jurnal ilmiah sesuai template jurnal rujukan <br>
+                        publikasi <br>
+                    </p>
+                    <p>
+                        Ketentuan: <br>
+                        Melampirkan file Bab I-V dari Skripsi, <br>
+                        Tesis atau Disertasi. <br>
+                    </p>
+                    <p>
+                    Biaya dalam pembuatan Jurnal berkisar antara <br>
+                    Rp. 300.000 - Rp. 800.000 <br>
+                    </p>
+                </div>
+                <div class="col-jr">
+                    <p>
+                        Bina Analitika membantu dalam publikasi <br>
+                        jurnal ilmiah baik indeks nasional <br>
+                        (Sinta 1, 2, 3, 4 dan 5) maupun indeks internasional (Q3) <br>
+                    </p>
+                    <p>
+                        Ketentuan: <br>
+                        melampirkan ringkasan Skripsi, Tesis atau <br>
+                        Disertasi, kemudian memilih indeks jurnal <br>
+                        yang iinginkan.
+                    </p>
+                </div>
+               
+            </div>
+            
+        </section>
+
+        <!-- ABOUT -->
+        <section class="about" id="about">
+            <div class="row-about">
+                <div class="col-about-img">
+                    <img src="img/fix-about-img.png" alt="none">
+                </div>
+                <div class="col-about">
+                    <h3>About Us</h3>
+                    <div class="content-about">
+                        <p>
+                            <b>Bina Analitika</b> merupakan instansi pendidikan yang memberikan layanan dalam proses
+                            penyusunan karya ilmiah baik Skripsi, Tesis maupun Disertasi.
+                            Bina Analitika membantu anda dalam menemukan konsep, jurnal yang
+                            relevan, referensi yang kredibel serta metode penelitian yang tepat.
+                        </p>
+                    </div>
+                    <div class="content-about">
+                        <p>
+                            <b>Bina Analitika</b> juga dapat membantu anda dalam proses pengumpulan data baik data
+                            primer
+                            seperti data kuesioner maupun data sekunder yang berasal dari laporan
+                            keuangan dan sejenisnya. Bina Analitika dapat membantu anda dalam
+                            proses analisis data secara statistik dengan berbagai metode
+                            statistik baik secara manual maupun menggunakan
+                            progam seperti SPSS, Eviews, Amos, SmartPLS.
+                        </p>
+                    </div>
+                </div>
+            </div>
+                <div class="row-about-foot">
+                    <div class="row-tag">
+                        <h6>©2023, Bina Analitika</h6>
+                    </div>
+                        <div class="contents">
+                            <div class = "cnt">
+                                <h5>Contact Us</h5>
+                            </div>
+                            <div class = "cnt">
+                                <div>
+                                    <a href="https://goo.gl/maps/YX9gwixNAj9EXBnFA" target="_blank"><img src="img/locator.png" alt="none"></a>
+                                </div>
+                                <div class = "text">
+                                <a href="https://goo.gl/maps/YX9gwixNAj9EXBnFA" target="_blank"><p>Jl. Tanjung Duren Raya No.64A, RT.4/RW.2, Tj. Duren Sel.</p></a>
+                                </div>
+                            </div>
+                        
+                            <div class = "cnt">
+                                <div>
+                                    <a href="#"><img src="img/logo-icons.png" alt="none"></a>
+                                </div>
+                                <div class = "text">
+                                    <p>bina.analitika@gmail.com</p>
+                                </div>
+                            </div>
+
+                            <div class = "cnt">
+                                <div>
+                                    <a href="#"><img src="img/smartphone.png" alt="none"></a>
+                                </div>
+                                <div class = "text">
+                                <p>081326852905</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="contents">
+                            <div class = "cnt">
+                                <h5>Rekening</h5>
+                                <br>
+                                <br>
+                            </div>
+                            <div class = "cnt">
+                                <div class = "text">
+                                    <h6>BCA</h6>
+                                </div>
+                            </div>
+                            <div class = "cnt">
+                                <div class = "text">
+                                    <h6>2390487714</h6>
+                                </div>
+                            </div>
+                            <div class = "cnt">
+                                <div class = "text">
+                                    <h6>Ropingun Muhlison</h6>
+                                </div>
+                                <br>
+                            </div>
+                        </div>
+                        
+                        <div class="contents">
+                            <div class="foot-header">
+                            <div class = "cnt">
+                                <h5>Pengunjung</h5>
+                                <br>
+                                <br>
+                                </div>
+                                <div class = "cnt">
+                                <pre>Hari ini   : <span id="counterDay">-</span></pre>
+                                </div>
+                                <div class = "cnt">
+                                <pre>Kemaren    : <span id="counterYesterday">-</span></pre>
+                                </div>
+                                <div class = "cnt">
+                                <pre>Minggu ini : <span id="counterWeek">-</span></pre>
+                                </div>
+                            </div>
+                        </div>
+                </div>
+        </section>
+
+        <script src="js/script.js"></script>
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+        <script>
+            function callStoreProcedure() {
+                $.ajax({
+                    type: "POST",
+                    url: "visit_day.php",
+                    data: { action: "call_procedure"},
+                    success: function(response) {
+                        console.log("Response dari call_procedure: ", response);
+                    }
+                });
+            }
+
+            $(document).ready(function() {
+                callStoreProcedure();
+            });
+        </script>
+
+        <script>
+            function getOneWeek() {
+                $.ajax({
+                    type: "POST",
+                    url: "visit_day.php",
+                    data: { action: "select_oneweek"},
+                    success: function(data) {
+                        console.log(data);
+                        $("#counterWeek").text(data);
+                    }
+                });
+            }
+
+            $(document).ready(function() {
+                getOneWeek();
+                setInterval(getOneWeek, 2000); // Update setiap detik
+            });
+        </script>
+
+        <script>
+            function getYeseterday() {
+                $.ajax({
+                    type: "POST",
+                    url: "visit_day.php",
+                    data: { action: "select_yesterday"},
+                    success: function(data) {
+                        console.log(data);
+                        $("#counterYesterday").text(data);
+                    }
+                });
+            }
+
+            $(document).ready(function() {
+                getYeseterday();
+                setInterval(getYeseterday, 2000); // Update setiap detik
+            });
+        </script> 
+
+        <script>
+            function getToday() {
+                $.ajax({
+                    type: "POST",
+                    url: "visit_day.php",
+                    data: { action: "select_today"},
+                    success: function(data) {
+                        console.log(data);
+                        $("#counterDay").text(data);
+                    }
+                });
+            }
+
+            $(document).ready(function() {
+                getToday();
+                setInterval(getToday, 2000); // Update setiap detik
+            });
+        </script>
+</body>
+
+</html>
